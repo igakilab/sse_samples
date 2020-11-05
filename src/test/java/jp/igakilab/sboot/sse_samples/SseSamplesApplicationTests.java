1c5fa48e187e60b7a18d3444b6691c58964c42e2
Hiroshi Igaki <igaki@github.com>
2020-11-05 10:03:11 +0900

    first

---
+++ src/test/java/jp/igakilab/sboot/sse_samples/SseSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.igakilab.sboot.sse_samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
